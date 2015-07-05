144a5e313df5badf80f8e1617b5fcac7c17e3875
Maciej Prus <macprus@gmail.com>
2015-07-05 21:25:40 +0200

    Fixed

--- src/main/java/com/acme/transmission/AutomaticTransmissionController.java
+++ src/main/java/com/acme/transmission/AutomaticTransmissionController.java
@@ -4,24 +4,7 @@ package com.acme.transmission;
  * Controlling module of the gear box
  */
 public class AutomaticTransmissionController {
-	public enum DrivingMode {
-		ECO(1000, 1500), COMFORT(1200, 1700), SPORT(2500, 5000), SPORT_PLUS(3500, 7000);
-		private int upperRpmLimit;
-		private int lowerRpmLimit;
 
-		private DrivingMode(int lowerRpmLimit, int upperRpmLimit) {
-			this.upperRpmLimit = upperRpmLimit;
-			this.lowerRpmLimit = lowerRpmLimit;
-		}
-
-		public int lowerLimit() {
-			return lowerRpmLimit;
-		}
-
-		public int upperLimit() {
-			return upperRpmLimit;
-		}
-	}
 
 	private AutomaticGearBox gearBox;
 	private EngineMonitoringSystem engineMonitoringSystem;
